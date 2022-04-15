9a73416da221127126d502efdbece938e2810d19
christianmaldonado <christian.zldr@gmail.com>
2022-04-15 14:09:58 -0400

    commit tree 8cd4aa56d97546028439d8515574e4839e32e1fd

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards) {
+    const allMessages = []
+    let i = 0; 
+    while (i < cards.length) {
+      console.log(`thank you ${cards[i]}, for the wonderful surprise gift!`);
+      allMessages.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`)
+      i++; 
+    }
+    console.log(allMessages)
+    return allMessages;
+  }
+  writeCards(cards);
+
+const down = 10
+
+function countDown(down){
+    for (let number = down ; number > -1; number--) {
+        console.log(number);
+      } 
+      return down;
+    }
+    countDown(down);
